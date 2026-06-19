26457d9f6821bd614a0ec6cbf1d859f03e48d976
Zubair <zubairabdulmatin@gmail.com>
2026-06-19 19:38:30 +0100

    Feature: Added pipes

--- src/shell.c
+++ src/shell.c
@@ -27,32 +27,36 @@ int main(void) {
             continue;
         }
 
-        char* token = strtok(input, " \t\n");
-        if (strcmp(token, "cd") == 0) {
-            char* fileDirectory = strtok(NULL, " \t\n");
-            if (fileDirectory == NULL) {
-                chdir(getenv("HOME"));
-            }else {
-                if (chdir(fileDirectory) == -1) {
-                    printf("Shell: No such directory or file exists.\n");
-                    continue;
-                }
-            }
-        }else if (strcmp(token, "exit") == 0){
-            exit(EXIT_SUCCESS);
-        }else {
-            int processID = fork();
+        char* pipe_pos = strchr(input, '|');
+        char* redirect_pos = strchr(input, '>');
 
-            if (processID < 0) {
-                printf("Shell: Fork failed\n");
+        if (pipe_pos != NULL) {
+            *pipe_pos = '\0';          // left command is now input
+            char *right = pipe_pos + 1; // right command starts here
+            char *left = input;
+
+            int fds[2];
+            pipe(fds);
+
+            // sub process 1 - handles output
+            int processID1 = fork();
+
+            if (processID1 < 0) {
+                printf("Shell: Fork Failed.\n");
                 exit(EXIT_FAILURE);
-            }else if (processID == 0) {
+            }else if (processID1 == 0) {
+                dup2(fds[1], STDOUT_FILENO);
+                close(fds[0]);
+                close(fds[1]);
+
+                left = strtok(left, " \t\n");
+
                 char *argv[64];
                 int argc = 0;
 
-                while (token != NULL && argc < 63) {
-                    argv[argc++] = token;
-                    token = strtok(NULL, " \t\n");
+                while (left != NULL && argc < 63) {
+                    argv[argc++] = left;
+                    left = strtok(NULL, " \t\n");
                 }
 
                 argv[argc] = NULL;  // null for execvp
@@ -64,8 +68,90 @@ int main(void) {
                     exit(EXIT_FAILURE);
                 }
 
+            }
+            
+            // sub process 2 - handles input
+            int processID2 = fork();
+            if (processID2 < 0) {
+                printf("Shell: Fork Failed.\n");
+                exit(EXIT_FAILURE);
+            }else if (processID2 == 0) {
+                dup2(fds[0], STDIN_FILENO);
+                close(fds[0]);
+                close(fds[1]);
+
+                right = strtok(right, " \t\n");
+
+                char *argv[64];
+                int argc = 0;
+
+                while (right != NULL && argc < 63) {
+                    argv[argc++] = right;
+                    right = strtok(NULL, " \t\n");
+                }
+
+                argv[argc] = NULL;  // null for execvp
+
+                char* command = argv[0];
+
+                if (execvp(command, argv) == -1) {
+                    printf("Shell: No such command\n");
+                    exit(EXIT_FAILURE);
+                }
+
+            }
+
+            // parent process
+
+            // closing file descriptors
+            close(fds[0]);
+            close(fds[1]);
+
+            // waiting for sub processes to finish
+            waitpid(processID1, NULL, 0);
+            waitpid(processID2, NULL, 0);
+
+        }else {
+            char* token = strtok(input, " \t\n");
+            if (strcmp(token, "cd") == 0) {
+                char* fileDirectory = strtok(NULL, " \t\n");
+                if (fileDirectory == NULL) {
+                    chdir(getenv("HOME"));
+                }else {
+                    if (chdir(fileDirectory) == -1) {
+                        printf("Shell: No such directory or file exists.\n");
+                        continue;
+                    }
+                }
+            }else if (strcmp(token, "exit") == 0){
+                exit(EXIT_SUCCESS);
             }else {
-                waitpid(processID, NULL, 0);
+                int processID = fork();
+
+                if (processID < 0) {
+                    printf("Shell: Fork failed\n");
+                    exit(EXIT_FAILURE);
+                }else if (processID == 0) {
+                    char *argv[64];
+                    int argc = 0;
+
+                    while (token != NULL && argc < 63) {
+                        argv[argc++] = token;
+                        token = strtok(NULL, " \t\n");
+                    }
+
+                    argv[argc] = NULL;  // null for execvp
+
+                    char* command = argv[0];
+
+                    if (execvp(command, argv) == -1) {
+                        printf("Shell: No such command\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                }else {
+                    waitpid(processID, NULL, 0);
+                }
             }
         }
 
