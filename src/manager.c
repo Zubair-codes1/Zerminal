2f8bb958b8bff409355c02c1f598dc1e123e8862
Zubair <zubairabdulmatin@gmail.com>
2026-06-22 19:11:17 +0100

    Finished parent process, input reading and PTY master reading and handled polling and terminal settings

--- src/manager.c
+++ src/manager.c
@@ -5,6 +5,8 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <stdbool.h>
+#include <termios.h>
 
 /**
  * Main function
@@ -15,7 +17,7 @@ int main(void) {
     int master_fd = posix_openpt(O_RDWR);
 
     if (master_fd == -1) {
-        printf("Error: PTY master not created.");
+        printf("Error: PTY master not created.\n");
         return EXIT_FAILURE;
     }
 
@@ -23,7 +25,7 @@ int main(void) {
     int grant_success = grantpt(master_fd);
 
     if (grant_success == -1) {
-        printf("Error: PTY master could not own PTY slave");
+        printf("Error: PTY master could not own PTY slave.\n");
         return EXIT_FAILURE;
     }
 
@@ -31,7 +33,7 @@ int main(void) {
     int unlock_success = unlockpt(master_fd);
 
     if (unlock_success == -1) {
-        printf("Error: PTY slave was not unlocked.");
+        printf("Error: PTY slave was not unlocked.\n");
         return EXIT_FAILURE;
     }
 
@@ -39,7 +41,7 @@ int main(void) {
     char* slave_path = ptsname(master_fd);
 
     if (slave_path == NULL) {
-        printf("Error: Null slave path.");
+        printf("Error: Null slave path.\n");
         return EXIT_FAILURE;
     }
 
@@ -48,7 +50,7 @@ int main(void) {
     pid_t childProcessID = fork();
 
     if (childProcessID < 0) {
-        printf("Error: Failed to setup child process (fork failed).");
+        printf("Error: Failed to setup child process (fork failed).\n");
         return EXIT_FAILURE;
     }else if (childProcessID == 0) {
 
@@ -56,7 +58,7 @@ int main(void) {
         pid_t sessionID = setsid();
 
         if (sessionID == -1) {
-            printf("Error: Session not created.");
+            printf("Error: Session not created.\n");
             exit(EXIT_FAILURE);
         }
 
@@ -64,14 +66,14 @@ int main(void) {
         int slave_fd = open(slave_path, O_RDWR);
 
         if (slave_fd == -1) {
-            printf("Error: Slave file path couldnt be opened.");
+            printf("Error: Slave file path couldnt be opened.\n");
             exit(EXIT_FAILURE);
         }
 
         int terminal_success = ioctl(slave_fd, TIOCSCTTY, 0);
 
         if (terminal_success == -1) {
-            printf("Error: Could not assign slave to be the controlling terminal.");
+            printf("Error: Could not assign slave to be the controlling terminal.\n");
             exit(EXIT_FAILURE);
         }
 
@@ -92,23 +94,80 @@ int main(void) {
         int close_success = close(slave_fd);
 
         if (close_success == -1) {
-            printf("Error: Could not close slave file descriptor.");
+            printf("Error: Could not close slave file descriptor.\n");
         }
 
         // executing the shell binary (zerminal)
-        char *shell_argv[] = {"shell", NULL};
+        char *shell_argv[] = {"./bin/shell", NULL};
 
-        int execution_success = execvp("../bin/zerminal", shell_argv);
+        int execution_success = execvp("./bin/shell", shell_argv);
 
         if (execution_success == -1) {
-            dprintf(safe_stderr, "Error: Zerminal execution failed.");
+            dprintf(safe_stderr, "Error: Zerminal execution failed.\n");
             exit(EXIT_FAILURE);
         }
 
 
     }else {
 
+        // original settings
+        struct termios original_settings;
+
+        tcgetattr(STDIN_FILENO, &original_settings);
+
+        // raw settings
+        struct termios raw_settings = original_settings;
+        raw_settings.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+        raw_settings.c_cc[VMIN] = 1;    // reads data instantly (1 keystroke)
+        raw_settings.c_cc[VTIME] = 0;   // does not wait
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &raw_settings);
+
+        // array of structs for watching input and master_fd (polling)
         struct pollfd fds[2];
+        int fds_size = 2;
+
+        fds[0].fd = STDIN_FILENO;
+        fds[0].events = POLLIN;     // any data available
+
+        fds[1].fd = master_fd;
+        fds[1].events = POLLIN;
+
+        // polling loop
+        while (true) {
+
+            // polls constantly for an activity
+            int activity = poll(fds, fds_size, -1);
+
+            if (activity == -1) {
+                perror("Poll error");
+                break;
+            }
+
+            // User input is read
+            if (fds[0].revents & POLLIN) {
+                char buffer[1024];
+                int bytes_read = read(fds[0].fd, buffer, sizeof(buffer));
+
+                write(master_fd, buffer, bytes_read);
+            }
+
+            // master_fd is read
+            if (fds[1].revents & POLLIN) {
+                char buffer[1024];
+                int bytes_read = read(fds[1].fd, buffer, sizeof(buffer));
+
+                if (bytes_read <= 0) {
+                    printf("[SHELL EXITED]\n");
+                    break;
+                }
+
+                write(STDOUT_FILENO, buffer, bytes_read);
+            }
+        }
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &original_settings);
     }
 
     return EXIT_SUCCESS;
