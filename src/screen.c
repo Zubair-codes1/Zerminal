3ac585d68d1a4a78c00680512b08066b2f953915
Zubair <zubairabdulmatin@gmail.com>
2026-06-25 13:11:34 +0100

    Feature: Started setting up the terminal screen

---
+++ src/screen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+
+// each cell
+typedef struct {
+    char character;     // actual character being held
+    uint32_t fg_colour; // foreground colour
+    uint32_t bg_colour; // background colour
+    uint8_t attrs;   // attributes
+} Cell;
+
+
+// Tracking Cursor
+typedef struct {
+    int x_pos;  // x pos
+    int y_pos;  // y pos
+} Cursor;
+
+
+// cols and rows for grid
+#define COLS 80
+#define ROWS 25
+
+// 2D array of cells
+Cell pixels[ROWS][COLS];
+
+// 256 characters with 16 bytes of length
+int characters[256][16];
+
