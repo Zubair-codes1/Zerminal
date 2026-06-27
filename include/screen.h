ee4d7404578b40037b1a46e3584a2b44ec1ca267
Zubair <zubairabdulmatin@gmail.com>
2026-06-27 18:42:35 +0100

    screen header file added

---
+++ include/screen.h
@@ -0,0 +1,30 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <stdint.h>
+#include <pthread.h>
+
+// cols and rows for grid
+#define COLS 80
+#define ROWS 25
+
+// each cell
+typedef struct {
+    char character;     // actual character being held
+    uint32_t fg_colour; // foreground colour
+    uint32_t bg_colour; // background colour
+    uint8_t attrs;      // attributes
+} Cell;
+
+// Tracking Cursor
+typedef struct {
+    int x_pos;  // x pos
+    int y_pos;  // y pos
+} Cursor;
+
+// Extern declarations: Tell everyone these variables exist globally
+extern Cell pixels[ROWS][COLS];
+extern Cursor terminal_cursor;
+extern pthread_mutex_t canvas_mutex;
+
+#endif
